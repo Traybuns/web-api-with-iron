069f8318817e51a38b1c38d1c605e59988175cfe
Traybuns <gobumbunrinmwa@gmail.com>
2024-12-16 12:51:58 +0100

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,66 @@ extern crate logger;
 extern crate router;
 extern crate uuid;
 
+mod models;
+mod databases;
+mod handlers;
 
+use models::*;
+use databases::Database;
+use handlers::*;
 use iron::prelude::*;
-use iron::status;
+use iron::Iron;
 use router::Router;
 use logger::Logger;
-use std::sync::Arc;
+use uuid::Uuid;
 
 fn main() {
+    // Initialize the logger
     env_logger::init();
 
-    let mut router = Router::new();
-
-    router.get("/", hello_world_handler, "index");
-    router.get("/api", api_handler, "api");
-
+    // Initialize logging middleware
     let (logger_before, logger_after) = Logger::new(None);
-    let chain = Arc::new(Chain::new(router));
+
+    // Create a new database
+    let mut db = Database::new();
+
+    // Add posts to the database
+    let p1 = Post::new(
+        "The First Post",
+        "This is the first post in our API",
+        "Traybuns",
+        chrono::Utc::now(),
+        Uuid::new_v4(),
+    );
+    db.add_post(p1);
+
+    let p2 = Post::new(
+        "The next post is better",
+        "Liquid Metal",
+        "Indaboski",
+        chrono::Utc::now(),
+        Uuid::new_v4(),
+    );
+    db.add_post(p2);
+
+    // Set up handlers
+    let handlers = Handlers::new(db);
+    let json_content_middleware = JsonAfterMiddleware;
+
+    // Define routes
+    let mut router = Router::new();
+    router.get("/post_feed", handlers.post_feed, "post_feed");
+    router.post("/post", handlers.post_post, "post_post");
+    router.get("/post/:id", handlers.post, "post");
+
+    // Set up middleware chain
+    let mut chain = Chain::new(router);
     chain.link_before(logger_before);
+    chain.link_after(json_content_middleware);
     chain.link_after(logger_after);
 
-    let address = "127.0.0.1:3000";
+    // Start the Iron server
+    let address = "localhost:8000";
     println!("Server running on http://{}", address);
     Iron::new(chain).http(address).unwrap();
-}
-
-fn hello_world_handler(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, "Hello, world!")))
-}
-
-fn api_handler(_: &mut Request) -> IronResult<Response> {
-    Ok(Response::with((status::Ok, r#"{"message": "Welcome to the API"}"#)))
 }
